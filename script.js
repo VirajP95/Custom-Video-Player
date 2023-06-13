0ccbb97a6807b51fedd652255c650810b21c18fd
Viraj Parab <virajparab.17@gmail.com>
2023-06-12 18:48:54 -0700

    Create Video Progress bar and Timestamp.

--- script.js
+++ script.js
@@ -25,12 +25,27 @@ function updatePlayicon() {
 
 // Change Playback progress
 function updateProgress() {
-  return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get Minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = "0" + String(mins);
+  }
+
+  //Get Seconds
+
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = "0" + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 //Set video time to progress
 function setVideoProgress() {
-  return true;
+  video.currentTime = (progress.value * video.duration) / 100;
 }
 
 // Stop Video using stop button
